e185c46dc2c6dbfa9025a11fa12d1fed52852da2
gis-song <test201514430123@126.com>
2021-05-12 09:34:23 +0200

    edit

--- iUrban/database.py
+++ iUrban/database.py
@@ -17,7 +17,7 @@ def connect_db():
     # # use the dbConfig.txt
 
     # # use this code in VS Code
-    myFile = open("E:\\PolimiCourseFiles\\MyCourses\\20202021semester2\\SE4geoinformatics\\gitProject\\Group1_Project\\iUrban\\dbConfig.txt", "r", encoding='utf-8')
+    myFile = open("E:\\path in your computer\\Group1_Project\\iUrban\\dbConfig.txt", "r", encoding='utf-8')
 
     # # use this code in Spyder
     # myFile = open("dbConfig.txt", "r", encoding='utf-8')
