a46994273ecb883d6079801d45950ea2914bf0b2
gis-song <test201514430123@126.com>
2021-05-15 19:32:43 +0200

    create

--- iUrban/app.py
+++ iUrban/app.py
@@ -10,23 +10,113 @@ from flask import (
     render_template,
     session,
     redirect,
-    url_for
+    url_for,
+    request,
+    flash
 )
+from werkzeug.security import (
+    check_password_hash,
+    generate_password_hash
+)
+
+from werkzeug.exceptions import abort
+
+from psycopg2 import connect
+
+from dbConfig import config
 
 app = Flask(__name__, template_folder='templates')
 
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = '_5#y2L"F4Q8z\n\xec]/'
 
+def connect_db():
+    # # use the dbConfig.txt
+
+    # # use this code in VS Code
+    myFile = open("E:\\PolimiCourseFiles\\MyCourses\\20202021semester2\\SE4geoinformatics\\gitProject\\Group1_Project\\iUrban\\dbConfig.txt", "r", encoding='utf-8')
+
+    # # use this code in Spyder
+    # myFile = open("dbConfig.txt", "r", encoding='utf-8')
+
+    connStr = myFile.readline()
+    conn = connect(connStr)
+    return conn
+
+
+
+    # # use the dbConfig.py
+    # params = config()    
+    # print('Connecting to the PostgreSQL database...')
+    # conn = connect(**params)
+    # return conn
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return redirect(url_for('index'))
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        else:
+            # params = config()
+            # conn = connect(**params)
+            conn = connect_db()
+            cur = conn.cursor()
+            cur.execute(
+                'SELECT user_id FROM TUser WHERE user_name = %s', (username,))
+            if cur.fetchone() is not None:
+                error = 'User {} is already registered.'.format(username)
+                cur.close()
+
+        if error is None:
+            # params = config()
+            # conn = connect(**params)
+            conn = connect_db()
+            cur = conn.cursor()
+            cur.execute(
+                'INSERT INTO TUser (user_name, user_password) VALUES (%s, %s)',
+                (username, generate_password_hash(password))
+            )
+            cur.close()
+            conn.commit()
+            return redirect(url_for('index'))
+
+        flash(error)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return redirect(url_for('index'))
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        error = None
+
+        conn = connect_db()
+        cur = conn.cursor()  # create a cursor
+        cur.execute(
+            'SELECT * FROM TUser WHERE user_name = %s', (username,)
+        )
+        user = cur.fetchone()
+        cur.close()  # close this cursor
+        conn.commit()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user[2], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user[0]
+            return redirect(url_for('index'))
+
+        flash(error)
 
 
 @app.route('/logout')
@@ -40,11 +130,45 @@ def base():
     return render_template('base.html')
 
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+
+@app.route('/table')
+def table():
+    conn = connect_db()
+    cur = conn.cursor()  # create a cursor
+    cur.execute(
+        """SELECT * FROM TData"""
+    )
+
+    tData = cur.fetchall()
+    cur.close()
+    conn.commit()
+
+    return render_template('table.html', page_title='Table', tData=tData)
+
+@app.route('/comment/<int:data_id>')
+def comment(data_id):
+    data_id = data_id
+    conn = connect_db()
+    cur = conn.cursor()  # create a cursor
+    cur.execute(
+        'SELECT * FROM TComment WHERE data_id = %s', (data_id,)
+    )
+
+    tComment = cur.fetchall()
+    cur.close()
+    conn.commit()
+
+    return render_template('comment.html', page_title=data_id, tComment=tComment)
 
 if __name__ == '__main__':
     app.run(
--- iUrban/database.py
+++ iUrban/database.py
@@ -13,6 +13,7 @@ from werkzeug.security import (
     generate_password_hash
 )
 
+
 def connect_db():
     # # use the dbConfig.txt
 
@@ -26,77 +27,76 @@ def connect_db():
     conn = connect(connStr)
     return conn
 
-
-
     # # use the dbConfig.py
-    # params = config()    
+    # params = config()
     # print('Connecting to the PostgreSQL database...')
     # conn = connect(**params)
     # return conn
 
 
-
 cleanup = (
-    'DROP TABLE IF EXISTS TUser CASCADE',
-    'DROP TABLE IF EXISTS TData',
+    'DROP TABLE IF EXISTS TUser CASCADE',    
     'DROP TABLE IF EXISTS TComment',
+    'DROP TABLE IF EXISTS TData',
 )
 
 commands = (
     """
     CREATE TABLE TUser (
         user_id SERIAL PRIMARY KEY,
-        user_name VARCHAR(255) UNIQUE NOT NULL,
-        user_password VARCHAR(255) NOT NULL
+        user_name VARCHAR(20) UNIQUE NOT NULL,
+        user_password VARCHAR(255) NOT NULL,
+        user_email varchar(30)
         )
     """,
+
     """
     CREATE TABLE TData (
         data_id SERIAL PRIMARY KEY,
         author_id INTEGER NOT NULL,
-        # title VARCHAR(255) NOT NULL, 
-        created_date TIMESTAMP DEFAULT NOW(), 
-        name VARCHAR(255) NOT NULL,
-        date VARCHAR(255) NOT NULL,
-        time VARCHAR(255) NOT NULL,            
-        latitude VARCHAR(255) NOT NULL,
-        longitude VARCHAR(255) NOT NULL,
-        average_noise_level VARCHAR(255) NOT NULL,
-        average_light_intensity VARCHAR(255) NOT NULL,
-        wind_direction VARCHAR(255) NOT NULL,
-        wind_speed VARCHAR(255) NOT NULL,
-        cloud_cover VARCHAR(255) NOT NULL,        
-        cloud_type VARCHAR(255) NOT NULL,
-        cloud_photo_id VARCHAR(255) NOT NULL,
-        visibility VARCHAR(255) NOT NULL,
-        traffic_count VARCHAR(255) NOT NULL,
-        temperature VARCHAR(255) NOT NULL,
-        humidity VARCHAR(255) NOT NULL,
-        collecting_photo_id VARCHAR(255) NOT NULL,        
-        note_of_anomaly VARCHAR(255) NOT NULL,
-        air_pollution VARCHAR(255) NOT NULL,    
+        created_date TIMESTAMP DEFAULT NOW(),
+        name VARCHAR(50) NOT NULL,
+        date VARCHAR(50) NOT NULL,
+        time VARCHAR(50) NOT NULL,
+        latitude FLOAT(16) NOT NULL,
+        longitude FLOAT(16) NOT NULL,
+        average_noise_level INTEGER NOT NULL,
+        average_light_intensity INTEGER NOT NULL,
+        wind_direction VARCHAR(16) NOT NULL,
+        wind_speed INTEGER NOT NULL,
+        cloud_cover INTEGER NOT NULL,
+        cloud_type VARCHAR(16) NOT NULL,
+        cloud_photo_id VARCHAR(64),
+        visibility INTEGER NOT NULL,
+        traffic_count INTEGER NOT NULL,
+        temperature INTEGER NOT NULL,
+        humidity INTEGER NOT NULL,
+        note_of_anomaly VARCHAR(255),
+        air_pollution INTEGER,
         FOREIGN KEY (author_id) REFERENCES TUser (user_id)
         )
     """,
+
     """
     CREATE TABLE TComment (
         comment_id SERIAL PRIMARY KEY,
         author_id INTEGER NOT NULL,
-        creation TIMESTAMP DEFAULT NOW(),
-        title VARCHAR(350) NOT NULL,
-        body VARCHAR(500) NOT NULL,
-        FOREIGN KEY (author_id) REFERENCES TUser (user_id)
+        data_id INTEGER NOT NULL,
+        created_date TIMESTAMP DEFAULT NOW(),
+        body VARCHAR(50) NOT NULL,
+        FOREIGN KEY (data_id) REFERENCES TData (data_id)
         )
     """
-    )
+)
 
 sqlCommands = (
     'INSERT INTO TUser (user_name, user_password) VALUES (%s, %s) RETURNING user_id',
-    
-    'INSERT INTO TData (author_id, title, name, date, time, longitude, latitude, average_noise_level, average_light_intensity, wind_direction, wind_speed, cloud_cover, cloud_type, cloud_photo_id, visibility, traffic_count, temperature, humidity, collecting_photo_id, note_of_anomaly, air_pollution) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
-    
-    'INSERT INTO TComment (author_id, title, body) VALUES (%s, %s, %s)'
-    )
+
+    'INSERT INTO TData (author_id,  name, date, time, latitude, longitude, average_noise_level, average_light_intensity, wind_direction, wind_speed, cloud_cover, cloud_type, cloud_photo_id, visibility, traffic_count, temperature, humidity, note_of_anomaly, air_pollution) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,  %s, %s, %s)',
+
+    'INSERT INTO TComment (author_id, data_id, body) VALUES (%s, %s, %s)'
+
+)
 
 conn = None
 try:
@@ -108,36 +108,24 @@ try:
 
     for command in commands:
         cur.execute(command)
-        
+
     # execute a statement
-    cur.execute(sqlCommands[0], ('yun', generate_password_hash('123456')))
     cur.execute(sqlCommands[0], ('song', generate_password_hash('123456')))
     cur.execute(sqlCommands[0], ('test', generate_password_hash('123456')))
     userId = cur.fetchone()[0]
-    cur.execute(sqlCommands[1], ('1', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('1', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('2', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('2', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('1', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('1', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('2', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('2', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('1', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('1', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('2', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('2', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('1', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('1', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('2', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('2', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('1', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('1', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('2', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[1], ('2', 'title', 'name', 'date', 'time', 'longitude', 'latitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'collecting_photo_id', 'note_of_anomaly', 'air_pollution'))
-    cur.execute(sqlCommands[2], ('1', 'Please!', 'leave a comment'))
-    cur.execute(sqlCommands[2], ('1', 'Please!', 'leave a comment'))
-    cur.execute(sqlCommands[2], ('2', 'Please!', 'leave a comment'))
-    cur.execute(sqlCommands[2], ('2', 'Please!', 'leave a comment'))
+
+    # cur.execute(sqlCommands[1], ('1', 'name', 'date', 'time', 'latitude', 'longitude', 'average_noise_level', 'average_light_intensity', 'wind_direction', 'wind_speed', 'cloud_cover', 'cloud_type', 'cloud_photo_id', 'visibility', 'traffic_count', 'temperature', 'humidity', 'note_of_anomaly', 'air_pollution'))
+
+    cur.execute(sqlCommands[1], (userId, 'macarena valdivia', '23/11/2020', '08:00:00', '-12.102838', '-77.039238', '57', '7164', 'NE', '4', '8', 'stratus', 'cloud_photo_id', '2', '102', '21', '68', 'note_of_anomaly', '9'))
+    
+    cur.execute(sqlCommands[1], (userId, 'song', '24/11/2020', '09:00:00', '-12.102838', '-77.039238', '57', '7164', 'NE', '4', '8', 'stratus', 'cloud_photo_id', '2', '102', '21', '68', 'note_of_anomaly', '9'))
+
+    cur.execute(sqlCommands[1], (userId, 'ding', '22/11/2020', '12:00:00', '-12.102838', '-77.039238', '50', '7164', 'NE', '4', '9', 'stratus', 'cloud_photo_id', '2', '100', '21', '68', 'note_of_anomaly', '9'))
+
+    cur.execute(sqlCommands[2], ('1', '1', 'leave a comment'))
+    cur.execute(sqlCommands[2], ('1', '1', 'leave a comment'))
+    cur.execute(sqlCommands[2], ('2', '1', 'leave a comment'))
+    cur.execute(sqlCommands[2], ('2', '2', 'leave a comment'))
 
     # print(cur.fetchall())
     cur.close()
